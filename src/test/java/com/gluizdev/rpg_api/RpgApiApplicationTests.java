c23261f1047fc74286d8a59d97bbcf111aad738b
Luiz Gustavo <gluizdev@gmail.com>
2025-11-25 20:05:46 -0400

    feat: Initial commit

---
+++ src/test/java/com/gluizdev/rpg_api/RpgApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gluizdev.rpg_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
